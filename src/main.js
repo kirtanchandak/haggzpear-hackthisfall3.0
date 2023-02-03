445b9c837c27f4bcb0abb2f3c58635ff6d41d561
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-04 00:23:25 +0530

    storyblock added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { StoryblokVue, apiPlugin } from "@storyblok/vue";
+import App from "./App.vue";
+import Grid from "./components/Grid.vue";
+import Page from "./components/Page.vue";
+import Teaser from "./components/Teaser.vue";
+import Feature from "./components/Feature.vue";
+import Test from "./components/Test.vue";
 
-import './assets/main.css'
+const app = createApp(App);
+app.component("Grid", Grid);
+app.component("Page", Page);
+app.component("Teaser", Teaser);
+app.component("Feature", Feature);
+app.component("Test", Test);
 
-createApp(App).mount('#app')
+app.use(StoryblokVue, {
+  accessToken: "xXqcPjdT6K1NrmXb23LFGgtt",
+  bridge: process.env.NODE_ENV !== "production", // optimizes by excluding the bridge on production
+  use: [apiPlugin],
+  apiOptions: {
+    timeout: 5,
+  },
+});
+
+app.mount("#app");
